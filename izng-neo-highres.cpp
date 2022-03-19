752c1653e892550cc9ce778334d0368f36b8e883
Katsumi Kougen <lesanonyme1001@gmail.com>
2022-03-19 19:11:48 +0700

    Included "splash.hpp"

--- izng-neo-highres.cpp
+++ izng-neo-highres.cpp
@@ -9,7 +9,7 @@
 #include <ctime>
 #include "splash.hpp"
 
-const int max_x = 50, max_y = 25;
+const int max_x = 100, max_y = 50;
 namespace color {
     short enum {Black, Red, Green, Yellow, Blue, Purple, Cyan, White, Bright};
 }
@@ -23,6 +23,7 @@ class Canvas
     struct {
         short x0 = 0, y0 = 0, x1 = 0, y1 = 0;
     } fill;
+    short fore = 7, back = 0;
 
     public:
         void init_color();
@@ -33,9 +34,9 @@ class Canvas
     private:
         void reset_canvas();
         void scan();
-        void fill_canvas(short col);
-        void fill_area(short col);
-        void draw_cursor();
+        void fill_canvas();
+        void fill_area(int clear);
+        //void draw_cursor();
         void keep_in_range();
         void print_status();
         void save();
@@ -45,30 +46,24 @@ class Canvas
 #define key_highlight_on attrset(COLOR_PAIR(1) | A_BOLD | A_REVERSE)
 #define key_highlight_off standend()
 #define print_splash(string) \
-    attrset(A_ITALIC | COLOR_PAIR(18)); \
+    attrset(A_ITALIC | COLOR_PAIR(66)); \
     mvprintw(27, 0, string); \
     standend()
 
 void Canvas::init_color(){
     init_pair(1, 15, 0); // default
-    init_pair(2, color::Black,  0);
-    init_pair(3, color::Red,    0);
-    init_pair(4, color::Green,  0);
-    init_pair(5, color::Yellow, 0);
-    init_pair(6, color::Blue,   0);
-    init_pair(7, color::Purple, 0);
-    init_pair(8, color::Cyan,   0);
-    init_pair(9, color::White,  0);
-    init_pair(10, color::Black+color::Bright,  0);
-    init_pair(11, color::Red+color::Bright,    0);
-    init_pair(12, color::Green+color::Bright,  0);
-    init_pair(13, color::Yellow+color::Bright, 0);
-    init_pair(14, color::Blue+color::Bright,   0);
-    init_pair(15, color::Purple+color::Bright, 0);
-    init_pair(16, color::Cyan+color::Bright,   0);
-    init_pair(17, color::White+color::Bright,  0);
-    init_pair(18, color::Yellow+color::Bright, 0); // splash
-    init_pair(19, color::Yellow+color::Bright, color::Red+color::Bright); // cursor
+    for (int i = 0;i<8;i++){
+        init_pair(8*i+2, color::Black,  i);
+        init_pair(8*i+3, color::Red,    i);
+        init_pair(8*i+4, color::Green,  i);
+        init_pair(8*i+5, color::Yellow, i);
+        init_pair(8*i+6, color::Blue,   i);
+        init_pair(8*i+7, color::Purple, i);
+        init_pair(8*i+8, color::Cyan,   i);
+        init_pair(8*i+9, color::White,  i);
+    }
+    init_pair(66, color::Yellow+color::Bright, 0); // splash
+    init_pair(67, color::Yellow+color::Bright, color::Red+color::Bright); // cursor
 }
 
 void Canvas::start(){
@@ -97,9 +92,9 @@ void Canvas::start(){
     for (int i = 0; i < 52; i++)
         mvprintw(26, i, ":");
 
-    mvprintw(0, 53, "▜▘▀▜ ▐▌▙▐ ▐▌▞▀▝▛    ▐▖▌▛▘▞▜");
-    mvprintw(1, 53, "▐ ▗▘ ▙▌▌▜ ▙▌▌▗ ▌ ▟█▘▐▝▌▛ ▌▐");
-    mvprintw(2, 53, "▟▖▙▄▐ ▌▌▐▐ ▌▙▟▗▙    ▐ ▌▙▖▙▞");
+    mvprintw(0, 53, "▜▘▀▜ ▐▌▙▐ ▐▌▞▀▝▛    ▐▖▌▛▘▞▜ |");
+    mvprintw(1, 53, "▐ ▗▘ ▙▌▌▜ ▙▌▌▗ ▌ ▟█▘▐▝▌▛ ▌▐ | high-res");
+    mvprintw(2, 53, "▟▖▙▄▐ ▌▌▐▐ ▌▙▟▗▙    ▐ ▌▙▖▙▞ |");
 
     key_highlight_on;mvprintw(4, 53, "WASD");
     key_highlight_off;mvprintw(4, 57, "/");
@@ -109,7 +104,7 @@ void Canvas::start(){
     key_highlight_on;mvprintw(6, 53, "ENTER");
     key_highlight_off;mvprintw(6, 58, "/");
     key_highlight_on;mvprintw(6, 59, "SPACEBAR");
-    key_highlight_off;mvprintw(6, 68, "plot the pixel");
+    key_highlight_off;mvprintw(6, 68, "plot / erase");
 
     key_highlight_on;mvprintw(8, 53, "1");
     key_highlight_off;mvprintw(8, 55, "black");
@@ -128,15 +123,15 @@ void Canvas::start(){
     key_highlight_on;mvprintw(11, 61, "8");
     key_highlight_off;mvprintw(11, 63, "white");
 
-    key_highlight_on;mvprintw(13, 53, "SHIFT");
-    key_highlight_off;mvprintw(13, 58, "+");
-    key_highlight_on;mvprintw(13, 59, "numkey");
-    key_highlight_off;mvprintw(13, 66, "use light colours");
+    key_highlight_on;mvprintw(13, 53, "<");
+    key_highlight_off;mvprintw(13, 55, "set fore");
+    key_highlight_on;mvprintw(13, 64, ">");
+    key_highlight_off;mvprintw(13, 66, "set back");
 
     key_highlight_on;mvprintw(15, 53, "9");
     key_highlight_off;mvprintw(15, 55, "fill an area");
     key_highlight_on;mvprintw(15, 68, "0");
-    key_highlight_off;mvprintw(15, 70, "fill the canvas");
+    key_highlight_off;mvprintw(15, 70, "clear the canvas");
 
     key_highlight_on;mvprintw(17, 53, "F1");
     key_highlight_off;mvprintw(17, 56, "save your work");
@@ -161,7 +156,6 @@ void Canvas::loop(){
     int key;
     while(1){
         scan();
-        draw_cursor();
         print_status();
         key = getch();
         switch (key){
@@ -197,26 +191,12 @@ void Canvas::loop(){
                 cursor.color = color::Cyan;break;
             case 56: // eight
                 cursor.color = color::White;break;
-            case 33:
-                cursor.color = color::Black+color::Bright;break;
-            case 64:
-                cursor.color = color::Red+color::Bright;break;
-            case 35:
-                cursor.color = color::Green+color::Bright;break;
-            case 36:
-                cursor.color = color::Yellow+color::Bright;break;
-            case 37:
-                cursor.color = color::Blue+color::Bright;break;
-            case 94:
-                cursor.color = color::Purple+color::Bright;break;
-            case 38:
-                cursor.color = color::Cyan+color::Bright;break;
-            case 42:
-                cursor.color = color::White+color::Bright;break;
             case 57: // nine
-                fill_area(cursor.color);break;
+                fill_area(0);break;
+            case 40:
+                fill_area(1);break;
             case 48: // zero
-                fill_canvas(cursor.color);break;
+                fill_canvas();break;
             case 91: // [
                 fill.x0 = cursor.curx;
                 fill.y0 = cursor.cury;
@@ -225,8 +205,18 @@ void Canvas::loop(){
                 fill.x1 = cursor.curx;
                 fill.y1 = cursor.cury;
                 break;
+            case 60: // <
+                fore = cursor.color;
+                break;
+            case 62: // >
+                back = cursor.color;
+                break;
             case 32: case 10: // enter / space
-                pixel[cursor.cury][cursor.curx] = cursor.color;break;
+                if (pixel[cursor.cury][cursor.curx] == 0)
+                    pixel[cursor.cury][cursor.curx] = 1;
+                else
+                    pixel[cursor.cury][cursor.curx] = 0;
+                break;
             case KEY_F(1): // F1
                 endwin();
                 save();
@@ -247,31 +237,31 @@ void Canvas::loop(){
 void Canvas::reset_canvas(){
     for (int y = 0; y < max_y; y++)
         for (int x=0; x < max_x; x++)
-            pixel[y][x] = color::Black;
+            pixel[y][x] = 0;
 }
 
 void Canvas::scan(){
-    for (int y = 0; y < max_y; y++)
-        for (int x = 0; x < max_x; x++){
-            switch (pixel[y][x]){
-                case color::Black:  attrset(COLOR_PAIR(2));break;
-                case color::Red:    attrset(COLOR_PAIR(3));break;
-                case color::Green:  attrset(COLOR_PAIR(4));break;
-                case color::Yellow: attrset(COLOR_PAIR(5));break;
-                case color::Blue:   attrset(COLOR_PAIR(6));break;
-                case color::Purple: attrset(COLOR_PAIR(7));break;
-                case color::Cyan:   attrset(COLOR_PAIR(8));break;
-                case color::White:  attrset(COLOR_PAIR(9));break;
-                case color::Black+color::Bright:  attrset(COLOR_PAIR(10));break;
-                case color::Red+color::Bright:    attrset(COLOR_PAIR(11));break;
-                case color::Green+color::Bright:  attrset(COLOR_PAIR(12));break;
-                case color::Yellow+color::Bright: attrset(COLOR_PAIR(13));break;
-                case color::Blue+color::Bright:   attrset(COLOR_PAIR(14));break;
-                case color::Purple+color::Bright: attrset(COLOR_PAIR(15));break;
-                case color::Cyan+color::Bright:   attrset(COLOR_PAIR(16));break;
-                case color::White+color::Bright:  attrset(COLOR_PAIR(17));break;
+    for (int y = 0; y < (short)(max_y/2); y++)
+        for (int x = 0; x < (short)(max_x/2); x++){
+            attrset(COLOR_PAIR(2+(8*back+fore)));
+            switch ((pixel[2*y][2*x] << 3) | (pixel[2*y][2*x+1] << 2) | (pixel[2*y+1][2*x] << 1) | pixel[2*y+1][2*x+1]){
+                case 0:  mvprintw(1+y, 1+x, " ");standend();break;
+                case 1:  mvprintw(1+y, 1+x, "▗");standend();break;
+                case 2:  mvprintw(1+y, 1+x, "▖");standend();break;
+                case 3:  mvprintw(1+y, 1+x, "▄");standend();break;
+                case 4:  mvprintw(1+y, 1+x, "▝");standend();break;
+                case 5:  mvprintw(1+y, 1+x, "▐");standend();break;
+                case 6:  mvprintw(1+y, 1+x, "▞");standend();break;
+                case 7:  mvprintw(1+y, 1+x, "▟");standend();break;
+                case 8:  mvprintw(1+y, 1+x, "▘");standend();break;
+                case 9:  mvprintw(1+y, 1+x, "▚");standend();break;
+                case 10: mvprintw(1+y, 1+x, "▌");standend();break;
+                case 11: mvprintw(1+y, 1+x, "▙");standend();break;
+                case 12: mvprintw(1+y, 1+x, "▀");standend();break;
+                case 13: mvprintw(1+y, 1+x, "▜");standend();break;
+                case 14: mvprintw(1+y, 1+x, "▛");standend();break;
+                case 15: mvprintw(1+y, 1+x, "█");standend();break;
             }
-            mvprintw(1+y, 1+x, "█");standend();
         }
     refresh();
 }
@@ -285,66 +275,95 @@ void Canvas::print_status(){
     key_highlight_off;mvprintw(23, 67, "%d %d  ", fill.x0, fill.y0);
     key_highlight_on;mvprintw(23, 73, "F1");
     key_highlight_off;mvprintw(23, 76, "%d %d  ", fill.x1, fill.y1);
-    key_highlight_on;mvprintw(24, 53, "COLOR");
+    key_highlight_on;mvprintw(24, 53, "FORE");
+    key_highlight_off;
+    switch (fore){
+        case color::Black:
+            mvprintw(24, 58, "black ");break;
+        case color::Red:
+            mvprintw(24, 58, "red   ");break;
+        case color::Green:
+            mvprintw(24, 58, "green ");break;
+        case color::Yellow:
+            mvprintw(24, 58, "yellow");break;
+        case color::Blue:
+            mvprintw(24, 58, "blue  ");break;
+        case color::Purple:
+            mvprintw(24, 58, "purple");break;
+        case color::Cyan:
+            mvprintw(24, 58, "cyan  ");break;
+        case color::White:
+            mvprintw(24, 58, "white ");break;
+    }
+    key_highlight_on;mvprintw(24, 65, "BACK");
+    key_highlight_off;
+    switch (back){
+        case color::Black:
+            mvprintw(24, 70, "black ");break;
+        case color::Red:
+            mvprintw(24, 70, "red   ");break;
+        case color::Green:
+            mvprintw(24, 70, "green ");break;
+        case color::Yellow:
+            mvprintw(24, 70, "yellow");break;
+        case color::Blue:
+            mvprintw(24, 70, "blue  ");break;
+        case color::Purple:
+            mvprintw(24, 70, "purple");break;
+        case color::Cyan:
+            mvprintw(24, 70, "cyan  ");break;
+        case color::White:
+            mvprintw(24, 70, "white ");break;
+    }
+    key_highlight_on;mvprintw(25, 53, "COLOUR");
     key_highlight_off;
     switch (cursor.color){
         case color::Black:
-            mvprintw(24, 59, "black       ");break;
+            mvprintw(25, 60, "black ");break;
         case color::Red:
-            mvprintw(24, 59, "red         ");break;
+            mvprintw(25, 60, "red   ");break;
         case color::Green:
-            mvprintw(24, 59, "green       ");break;
+            mvprintw(25, 60, "green ");break;
         case color::Yellow:
-            mvprintw(24, 59, "yellow      ");break;
+            mvprintw(25, 60, "yellow");break;
         case color::Blue:
-            mvprintw(24, 59, "blue        ");break;
+            mvprintw(25, 60, "blue  ");break;
         case color::Purple:
-            mvprintw(24, 59, "purple      ");break;
+            mvprintw(25, 60, "purple");break;
         case color::Cyan:
-            mvprintw(24, 59, "cyan        ");break;
+            mvprintw(25, 60, "cyan  ");break;
         case color::White:
-            mvprintw(24, 59, "light gray  ");break;
-        case color::Black+color::Bright:
-            mvprintw(24, 59, "gray        ");break;
-        case color::Red+color::Bright:
-            mvprintw(24, 59, "light red   ");break;
-        case color::Green+color::Bright:
-            mvprintw(24, 59, "light green ");break;
-        case color::Yellow+color::Bright:
-            mvprintw(24, 59, "light yellow");break;
-        case color::Blue+color::Bright:
-            mvprintw(24, 59, "light blue  ");break;
-        case color::Purple+color::Bright:
-            mvprintw(24, 59, "pink        ");break;
-        case color::Cyan+color::Bright:
-            mvprintw(24, 59, "light cyan  ");break;
-        case color::White+color::Bright:
-            mvprintw(24, 59, "white       ");break;
+            mvprintw(25, 60, "white ");break;
     }
     standend();
     refresh();
 }
 
-void Canvas::fill_canvas(short col){
+void Canvas::fill_canvas(){
     for (int y = 0; y < max_y; y++)
         for (int x = 0; x < max_x; x++)
-            pixel[y][x] = col;
+            pixel[y][x] = 0;
 }
 
-void Canvas::fill_area(short col){
+void Canvas::fill_area(int clear){
     int starty = std::min(fill.y0, fill.y1), endy = std::max(fill.y0, fill.y1) + 1;
     int startx = std::min(fill.x0, fill.x1), endx = std::max(fill.x0, fill.x1) + 1;
-    for(int y = starty; y < endy; ++y)
-        for(int x = startx; x < endx; ++x)
-            pixel[y][x] = col;
+    if (clear == 0)
+        for(int y = starty; y < endy; ++y)
+            for(int x = startx; x < endx; ++x)
+                pixel[y][x] = 1;
+    else
+        for(int y = starty; y < endy; ++y)
+            for(int x = startx; x < endx; ++x)
+                pixel[y][x] = 0;
 }
 
-void Canvas::draw_cursor(){
+/*void Canvas::draw_cursor(){
     attrset(COLOR_PAIR(19));
     mvprintw(1+cursor.cury, 1+cursor.curx, "▒");
     standend();
     refresh();
-}
+}*/
 
 void Canvas::keep_in_range(){
     if (cursor.curx < 0) cursor.curx = 0;
@@ -355,7 +374,7 @@ void Canvas::keep_in_range(){
 
 void Canvas::save(){
     char infilename[0xfff];
-    std::cout << "enter file name (example: owo.iz2): ";
+    std::cout << "enter file name (example: owo.iz2x): ";
     std::cin >> infilename;
 
     std::ofstream outf{infilename};
@@ -364,22 +383,8 @@ void Canvas::save(){
         for (int y = 0; y < max_y; y++)
             for (int x = 0; x < max_x; x++)
                 switch (pixel[y][x]){
-                    case color::Black:  outf << "0";break;
-                    case color::Red:    outf << "1";break;
-                    case color::Green:  outf << "2";break;
-                    case color::Yellow: outf << "3";break;
-                    case color::Blue:   outf << "4";break;
-                    case color::Purple: outf << "5";break;
-                    case color::Cyan:   outf << "6";break;
-                    case color::White:  outf << "7";break;
-                    case color::Black+color::Bright:  outf << "8";break;
-                    case color::Red+color::Bright:    outf << "9";break;
-                    case color::Green+color::Bright:  outf << "a";break;
-                    case color::Yellow+color::Bright: outf << "b";break;
-                    case color::Blue+color::Bright:   outf << "c";break;
-                    case color::Purple+color::Bright: outf << "d";break;
-                    case color::Cyan+color::Bright:   outf << "e";break;
-                    case color::White+color::Bright:  outf << "f";break;
+                    case 0: outf << "0";break;
+                    case 1: outf << "1";break;
         }
         outf.close();
     }
@@ -390,11 +395,11 @@ void Canvas::save(){
 
 void Canvas::load(){
     char outfilename[0xfff];
-    std::cout << "enter file name (example: owo.iz2): ";
+    std::cout << "enter file name (example: owo.iz2x): ";
     std::cin >> outfilename;
 
     std::ifstream inf{outfilename};
-    char data[1250];
+    char data[5000];
     if (!inf){
         std::cerr << "welp that sucks.\nthe program couldn't open the file.";
         sleep(2);
@@ -405,22 +410,8 @@ void Canvas::load(){
             for (int y = 0; y < max_y; y++)
                 for (int x = 0; x < max_x; x++)
                     switch (data[max_x*y+x]){
-                        case (char)48: pixel[y][x] = color::Black;break;
-                        case (char)49: pixel[y][x] = color::Red;break;
-                        case (char)50: pixel[y][x] = color::Green;break;
-                        case (char)51: pixel[y][x] = color::Yellow;break;
-                        case (char)52: pixel[y][x] = color::Blue;break;
-                        case (char)53: pixel[y][x] = color::Purple;break;
-                        case (char)54: pixel[y][x] = color::Cyan;break;
-                        case (char)55: pixel[y][x] = color::White;break;
-                        case (char)56: pixel[y][x] = color::Black+color::Bright;break;
-                        case (char)57: pixel[y][x] = color::Red+color::Bright;break;
-                        case (char)97: pixel[y][x] = color::Green+color::Bright;break;
-                        case (char)98: pixel[y][x] = color::Yellow+color::Bright;break;
-                        case (char)99: pixel[y][x] = color::Blue+color::Bright;break;
-                        case (char)100: pixel[y][x] = color::Purple+color::Bright;break;
-                        case (char)101: pixel[y][x] = color::Cyan+color::Bright;break;
-                        case (char)102: pixel[y][x] = color::White+color::Bright;break;
+                        case (char)48: pixel[y][x] = 0;break;
+                        case (char)49: pixel[y][x] = 1;break;
                     }
             inf.close();
         }
@@ -435,7 +426,7 @@ int main(){
     std::cout << "▜▘▀▜ ▐▌▙▐ ▐▌▞▀▝▛    ▐▖▌▛▘▞▜  |  written in C++ by Katsumi Kougen\n"
                  "▐ ▗▘ ▙▌▌▜ ▙▌▌▗ ▌ ▟█▘▐▝▌▛ ▌▐  |  github.com/KatsumiKougen\n"
                  "▟▖▙▄▐ ▌▌▐▐ ▌▙▟▗▙    ▐ ▌▙▖▙▞  |  this software is free, as in \"\x1b[4mfree\x1b[0m candy,  kids!\"\n"
-                 "                             |  special thanks to: Warp, Bisqwit\n\n"
+                 "  high resolution version    |  special thanks to: Warp, Bisqwit\n\n"
                  "initialising \x1b[1mizanagi-neo\x1b[0m...\n";
     Canvas cv;
     setlocale(LC_ALL, "");
