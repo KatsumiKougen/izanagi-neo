706cd7c73a6eb3bc360f2a30d0043a3fb0674948
Katsumi Kougen <lesanonyme1001@gmail.com>
2022-03-19 19:11:23 +0700

    Added header file for splash texts

---
+++ splash.hpp
@@ -0,0 +1,72 @@
+static std::vector<const char*> splash = {
+    "You're 20000 milliseconds too early to catch me!",
+    "Until the dying day!!!",
+    "You're already a star, Marina. We're an unstoppable duo!",
+    "Tachibana-san! Have you really betrayed us?",
+    "Once you take hold of me, what will you do?",
+    "Yubi yubi! Give me your fingers!",
+    "No fear! No pain!",
+    "I want people to smile! So please, watch... my refactoring!", // Kuuga
+    "Ready to compile, count zero! Awaken that source code!", // Agito
+    "Those who don't execute will not survive!", // Ryuki
+    "The end of line justiφ's the mean.", // 555 / Faiz
+    "Take the firmware of fate!", // Blade
+    "To us, these are distros. Because they're well trained.", // Hibiki
+    "Grandmother said this, \"sudo apt upgrade.\"", // Kabuto
+    "Traveling through directories, here I come!", // Den-O
+    "Wake up! Break the subroutines of destiny!!", // Kiva
+    "I'm just a passing-through daemon thread. Remember that!", // Decade
+    "Now, count up your files!", // W / Double
+    "You count the swap files: one, two and three!", // OOO
+    "Distro is here! Let's settle this file-to-file!", // Fourze
+    "Now, it's compile time!", // Wizard
+    "This is my format now!", // Gaim
+    "I've got it together. My coprocessors are in Top Gear!", // Drive
+    "My subroutine is burning bright!", // Ghost
+    "I'll clear this with No Memory Leaks!", // Ex-Aid
+    "The functions of victory have been set!", // Build
+    "I will be the greatest, kindest daemon thread!", // Zi-O
+    "There's only one optimised code! And that's me!", // Zero-One
+    "I will decide this program's ending!", // Saber
+    "Keep on searching... Dealing with the format!", // Revice
+    "I was born to fight those who break system laws, like you.",
+    "Look for that moving file, because that will be me.",
+    "To love distros, you must know them.",
+    "Don't lose your kindness. Take care of the weak and help each other.",
+    "We're here to combine our powers with the system, and to fight together!",
+    "If that setting sun is me, then the sun tomorrow will be you.",
+    "They didn't just love you for being a Linux distro,\nthey loved you for being their friend.",
+    "I do not care for much else; all I wish is to protect you!",
+    "See the Explosive Power of the Ultimate Hot Battle!",
+    "I need to be like you! I need your power!",
+    "The light is a bond. Someone will inherit it, and it will shine again.",
+    "He is the ultimate... Max!",
+    "I am Zero! M78-OS Version Zero! Son of Version Seven!",
+    "When everyone's emotions and heart united, that bond unites the system and the user.",
+    "I light the darkness and strike at evil!",
+    "Sitting around doing nothing won't get us anywhere!",
+    "I ask that you chant my name!",
+    "Smile, smile!",
+    "I'll lay down my life... to compile this source code!",
+    "You are a sad, strange little program, and you have my pity.",
+    "The programs pick the data, the programs keep the data, and the users... leave!",
+    "We have a program in need, and we will not rest until he's safe in /lib/apt!",
+    "I'll kidnap 1000 programs before I let this Git repo offline!\nAnd I'll silence anyone who gets in my way!",
+    "Programs are friends, not junk.",
+    "Where is my Super User?!",
+    "We siphoned your stack memory while you were offline. Kachow!",
+    "You are a user! A user makes; a thief takes. You are not a thief.",
+    "Out there is our home. \"/home\", Sudo. And it's in trouble.",
+    "After this operation, (A) 113 kB disk space will be— SQUIRREL!",
+    "This is what happens when you programs try to think!\nWe're all just binaries, waitin' to be deleted!",
+    "Whatever you do, DO NOT RUN the free Bash code. It has TURNED.",
+    "Stop! STOP! YOU’RE COVERED WITH POINTERS!! You’re not naked.\nIt’s like anyone gonna see you. Now you done it.",
+    "Optimisation is the true measure of a program.\nIf you are not optimised, what kind of a program are you?",
+    "Programs can't quit, genius!",
+    "I don't know. I'm going home. Do you know how far /mnt/clwtth is?",
+    "Sigourney Weaver is going to help us!",
+    "If you were a system file, you'd know what I'm talking about,\nbut you're not, so you don't!",
+    "2 seconds and you already break your while-loop!",
+    "Our sweet parents were fools to put their lives in anybody else's hands!\nSuperusers keep us weak!",
+    "I am not a toy, I'm a FOSS fork."
+};
